835e6a969ce59b6b64fdebeeb7b52d78fc0a297b
Patrick H Li <patvol606@gmail.com>
2026-01-02 23:55:19 -0500

    created new branch

--- Client/Scholar_Stay/src/pages/BookmarkPage.jsx
+++ Client/Scholar_Stay/src/pages/BookmarkPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import HouseCard from '../components/HouseCard'; // Assuming HouseCard is reusable
-import './BookmarksPage.css';
+import './BookmarkPage.css';
 
 const base_url = "http://127.0.0.1:8000/api/v1/";
 
